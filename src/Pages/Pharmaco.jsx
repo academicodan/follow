63cbe1f2c6e29a7a051a0532dd326f107939434b
pedrocarrijo <pedro.carrijo@oracle.com>
2021-11-08 08:36:12 -0300

    adicionado functions blockchain

--- src/Pages/Pharmaco.jsx
+++ src/Pages/Pharmaco.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { useForm, FormActions } from "../context/FormContext";
 import { Theme } from "./../Components/Theme";
-
+import axios from "axios";
 export const Pharmaco = () => {
   const [codigoChaveFarmaco, setCodigoChaveFarmaco] = useState("");
   const [tipo, setTipo] = useState("");
@@ -21,6 +21,30 @@ export const Pharmaco = () => {
 
   const handleNextStep = () => {
     console.table({ codigoChaveFarmaco, tipo, peso });
+    //1. CODAR AQUI
+    const timeElapsed = Date.now();
+    const today = new Date(timeElapsed);
+    var datetimenow = today.toLocaleDateString() + " " + today.toLocaleTimeString();
+    var data = JSON.stringify({"channel":"pucchannel","chaincode":"oabcs-medicine-puc","chaincodeVer":"v2","method":"addItemFarmaco","args":[codigoChaveFarmaco,datetimenow,tipo,peso]});
+
+    var config = {
+      method: 'post',
+      url: 'https://blockhubiteam-ladcsteam-iad.blockchain.ocp.oraclecloud.com:7443/restproxy/bcsgw/rest/v1/transaction/invocation',
+      headers: { 
+        'Authorization': 'Basic cGVkcm8uYmxvY2tjaGFpbjpCbG9jayYxMjQ0MjkxMg==', 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
     dispatch({
       type: FormActions.setValidationStepPharmaco,
       payload: true,
