3ba15595bb23f7f85cf590a8df17faef5a242fa3
Daniel Teixeira Ribeiro <academicodan@gmail.com>
2022-11-03 21:10:41 -0300

    fix case

---
+++ src/Context/hook/useAuth.js
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { handleAuthentication } from '../../General/helpers'
+import history from '../../history'
+
+const useAuth = () => {
+    const [authenticated, setAuthenticated] = useState(false)
+    const handleLogin = (user) => {
+        const userName = user.user
+        const password = user.password
+        const result = handleAuthentication({ userName, password })
+        setAuthenticated(result.authenticated)
+        history.push(result.route)
+    }
+
+    const handleLogaut = () => {
+        setAuthenticated(false)
+        history.push('/')
+    }
+
+    return { authenticated, handleLogin, handleLogaut }
+}
+
+export default useAuth
